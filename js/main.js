95fe7451cc775a3a1189a2433e40a2ee359dddf7
alexwhitedev <alexwhitedeveloper@gmail.com>
2019-02-09 03:03:14 +0200

    fornt v2.0

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.sign__up_href').click(function(e){
+         
+    });
+
+    $('.sign__in_href').click(function(e){
+        
+    });
+
+    $('.log_href').click(function(e){
+        
+    });
+
+    $('.reg_href').click(function(e){
+        
+    });
+});
